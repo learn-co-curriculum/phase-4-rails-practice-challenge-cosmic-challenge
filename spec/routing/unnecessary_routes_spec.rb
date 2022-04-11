7d9cb1ee5408c55bbc8a00bc81c0ef3461d58fd5
betalantz <betalantz@gmail.com>
2022-04-11 13:45:26 -0700

    adds routes specs

--- spec/routing/unnecessary_routes_spec.rb
+++ spec/routing/unnecessary_routes_spec.rb
@@ -2,22 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "UnnecessaryRoutes", type: :routing do
   
-  it "does not define unnecessary campers routes" do
-    expect(patch: "/campers/1").not_to be_routable
-    expect(delete: "/campers/1").not_to be_routable
+  it "does not define unnecessary scientists routes" do
+    expect(new: "/campers/1").not_to be_routable
+    expect(edit: "/campers/1").not_to be_routable
   end
   
-  it "does not define unnecessary activities routes" do
-    expect(post: "/activities").not_to be_routable
-    expect(get: "/activities/1").not_to be_routable
-    expect(patch: "/activities/1").not_to be_routable
+  it "does not define unnecessary planets routes" do
+    expect(post: "/planets").not_to be_routable
+    expect(get: "/planets/1").not_to be_routable
+    expect(patch: "/planets/1").not_to be_routable
+    expect(delete: "/planets/1").not_to be_routable
   end
   
-  it "does not define unnecessary signups routes" do
-    expect(get: "/signups").not_to be_routable
-    expect(get: "/signups/1").not_to be_routable
-    expect(patch: "/signups/1").not_to be_routable
-    expect(delete: "/signups/1").not_to be_routable
+  it "does not define unnecessary missions routes" do
+    expect(get: "/missions").not_to be_routable
+    expect(get: "/missions/1").not_to be_routable
+    expect(patch: "/missions/1").not_to be_routable
+    expect(delete: "/missions/1").not_to be_routable
   end
 
 end
