834225fa60f747dd87f301f7521622a167fd8a28
betalantz <betalantz@gmail.com>
2022-04-13 09:57:15 -0700

    adds messages to seeds

--- client/src/components/ScientistCard.jsx
+++ client/src/components/ScientistCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function ScientistCard({scientist: {id, name, avatar}, onDelete}) {
   return (
     <div className="scicard">
-        <img src={avatar} alt={name} />
+        <img src={avatar} alt={name} load="lazy" width="100px" height="100px"/>
         <h3>{name}</h3>
         <Link to={`/scientists/${id}`}>View Missions</Link>
         <button onClick={()=>onDelete(id)}>X</button>
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Clearing db..."
 Planet.destroy_all
 Scientist.destroy_all
 Mission.destroy_all
@@ -13,15 +14,20 @@ Faker::TvShows::StarTrek.unique.clear
 Faker::Space.unique.clear
 Faker::TvShows::Buffy.unique.clear
 
+puts "Making planets..."
 20.times {Planet.create(name: Faker::TvShows::StarTrek.unique.location,
                         distance_from_earth: Faker::Space.unique.distance_measurement,
                         nearest_star: Faker::Space.star,
                         image: "planet#{rand(1..10)}")}
 
+puts "Making scientists..."                        
 15.times {Scientist.create(name: Faker::FunnyName.name,
                            field_of_study: Faker::Educator.subject,
-                           avatar: Faker::Avatar.image(set: "set5"))}
+                           avatar: Faker::Avatar.image(size: "200x200", set: "set5"))}
 
+puts "Making missions..."                           
 20.times {Mission.create(name: Faker::TvShows::Buffy.unique.episode,
                          scientist: Scientist.all.sample, 
                          planet: Planet.all.sample)}
+
+puts "Done seeding!"                         
